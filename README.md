0bd6d0e87574afaa08aca51ad9bbd421215efad0
Jack Merrill <me@jackmerrill.com>
2022-04-08 13:30:08 -0500

    initial commit :tada:

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# galaxy
-A simple file server written in Go.
+<h1 align="center"><img src="./Comet.png" width="100" /></h1>
+<h1 align="center">Comet</h1>
+<p align="center">A simple and fast web-powered fileserver written in Go.</p>
+
+## Deployment
+We recommend using Docker for deployment.
+```
+$ docker run -d -p 8080:8080 -v /path/to/comet/files:/files gcr.io/astralservices/comet:latest
+```
+
+### Traefik
+We recommend using Traefik for reverse-proxying.
+
+You can also use `rsync` to sync your files to multiple nodes.
+```
+$ rsync -avz /path/to/comet/files/ node1:/path/to/comet/files/
+```
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	var dir string
+	err := godotenv.Load(".env") // dev purposes
+
+	if err != nil {
+		log.Fatalf("Some error occured while loading .env file %s", err)
+	}
+
+	flag.StringVar(&dir, "dir", os.Getenv("COMET_ROOT./"), "the directory to serve files from. Defaults to the current dir")
+	flag.Parse()
+	r := mux.NewRouter()
+
+	r.PathPrefix("/files/").Handler(http.StripPrefix("/files/", http.FileServer(http.Dir(dir))))
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    ":" + os.Getenv("COMET_PORT"),
+
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Println("Server started at " + "http://127.0.0.1:" + os.Getenv("COMET_PORT"))
+
+	log.Fatal(srv.ListenAndServe())
+}
